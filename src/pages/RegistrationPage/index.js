701a545c32fae9d728b51c596a3191616430ea8b
k <kirill.chechkenev@rtkit.ru>
2022-08-10 00:55:06 +0400

    add signin and signup pages

---
+++ src/pages/RegistrationPage/index.js
@@ -0,0 +1,6 @@
+import renderPageTemplate from './RegistrationPage.hbs';
+import * as s from '../styles/SignPages.module.css';
+
+// import buttonTemplate from '../../components/Button';
+
+export default renderPageTemplate({title: 'Регистрация', s});
